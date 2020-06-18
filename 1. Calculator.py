d46faccc7eb09dc3c3f6de6b4899750bd5a82fa2
pfdah <pratikdahal700@gmail.com>
2020-06-18 12:32:38 +0545

    Well

---
+++ 1. Calculator.py
@@ -0,0 +1,30 @@
+import re
+
+print('Welcome Hooman\nThis is a calculator')
+print('Type \'quit\' to exit')
+
+run = True
+previous  = 0
+
+
+def calc():
+    global run
+    global previous
+    if previous == 0:
+        eqn = input('Enter the required equation : \n').strip().lower()
+    else:
+        eqn = input(str(previous))
+    if eqn == 'quit':
+        print('Bye bye Hooman')
+        run = False
+    else:
+        eqn = re.sub('[a-zA-Z;,:"\']','',eqn)
+       
+        if previous == 0:
+            previous = eval(eqn)
+        else:
+            previous = eval(str(previous)+eqn)
+        print(previous)
+
+while run:
+    calc()
